c54056ae0996ea509746cc52c071a247d04ec329
hamzabeizig <hamza.beizig@esprit.tn>
2022-05-09 16:19:29 +0100

    beizig commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,16 +3,24 @@ import { RouterModule, Routes } from '@angular/router';
 import { EventsComponent } from './Pages/events/events.component';
 import { HomeComponent } from './Pages/home/home.component';
 import {AcceuilComponent} from "./Pages/acceuil/acceuil.component";
+import {UserComponent} from "./Pages/user/user.component";
+import {AdduserComponent} from "./Pages/adduser/adduser.component";
+import {UpdateuserComponent} from "./Pages/updateuser/updateuser.component";
+import {LoginComponent} from "./Pages/login/login.component";
+import {ProfileComponent} from "./Pages/profile/profile.component";
 
 const routes: Routes = [
   {
     path: 'home',
     component: HomeComponent,
     children: [{ path: 'events', component: EventsComponent } ,
-      {path: 'acceuil', component: AcceuilComponent }],
-  },
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
+      {path: 'acceuil', component: AcceuilComponent } ,
+      {path:'profile',component:ProfileComponent},
+      {path: 'user', component: UserComponent , children:[{path:'adduser',component:AdduserComponent},{path:'updateuser/:iduser',component:UpdateuserComponent}] }],
 
+},
+  {path:"login", component:LoginComponent},
+  {path:"", redirectTo:"/login", pathMatch:"full"}
 ];
 
 @NgModule({
